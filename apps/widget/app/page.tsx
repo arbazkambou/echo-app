413f49ed07742489e1de7bdc16d903ccfa952f22
Arbaz Shoukat <arbazkamboh342@gmail.com>
2026-01-21 12:35:13 +0500

    02-convex-package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Apps/widget</h1>
+        <h1 className="text-2xl font-bold">Apps/widget 2</h1>
         {JSON.stringify(users)}
       </div>
       <Button onClick={() => addUser()}>Add User</Button>
