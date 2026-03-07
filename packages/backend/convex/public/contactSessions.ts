43c4c50bb0bd97762a6609afe5cf2c561d26be2c
Arbaz Shoukat <arbazkamboh342@gmail.com>
2026-03-08 03:03:06 +0500

    completed

--- packages/backend/convex/public/contactSessions.ts
+++ packages/backend/convex/public/contactSessions.ts
@@ -1,7 +1,7 @@
 import { v } from "convex/values";
 import { mutation } from "../_generated/server";
 
-const SESSION_DURATION_MS = 8000;
+const SESSION_DURATION_MS = 24 * 60 * 60 * 1000;
 
 export const createContactSession = mutation({
   args: {
