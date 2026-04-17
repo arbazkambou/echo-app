3ea32bdeb7e1011ce6a02ffbba6987e28c5c7ceb
Arbaz Shoukat <arbazkamboh342@gmail.com>
2026-04-18 00:36:05 +0500

    made changes in widget script

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,7 +2,16 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/conversations",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
