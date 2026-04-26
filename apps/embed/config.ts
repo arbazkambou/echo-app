2d765a39fb1431722faa2381e1fcdb2f5f66e9b0
Arbaz Shoukat <arbazkamboh342@gmail.com>
2026-04-26 15:12:26 +0500

    change widget url

--- apps/embed/config.ts
+++ apps/embed/config.ts
@@ -1,5 +1,5 @@
 export const EMBED_CONFIG = {
-  WIDGET_URL: "https://echo-app-widget-ten.vercel.app",
+  WIDGET_URL: "https://aero-widget.vercel.app",
   DEFAULT_ORG_ID: "org_31QtvqJKwhtvop04esLJMkmFouB",
   DEFAULT_POSITION: "bottom-right" as const,
 };
